3e27c8df66b920637770e5bdc2c0696dcfb7be2d
Prince Csaba <bolyaicsaba@gmail.com>
2025-02-05 10:29:58 +0100

    -

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { createProject } = require('../controllers/createProject');
 const { getUserProjects } = require('../controllers/getUserProjects');
 const { createTask } = require('../controllers/createTask');
+const { getTasksOfProject } = require('../controllers/getTasksOfProject');
 const router = express.Router();
 
 //const { getTrips } = require('../controllers/GetAllTrips');
@@ -14,6 +15,6 @@ router.post('/projects/new', createProject);
 router.post('/projects/getUserProjects', getUserProjects);
 
 router.post('/tasks/new', createTask);
-
+router.post('/tasks/getTasksOfProject', getTasksOfProject);
 
 module.exports = router;
