28ea6a049483455696739d96e059f491ce1dd50e
Prince Csaba <bolyaicsaba@gmail.com>
2025-02-28 09:07:52 +0100

    refactor updateProject.js to support project updates with findByIdAndUpdate

--- controllers/updateProject.js
+++ controllers/updateProject.js
@@ -2,14 +2,18 @@ const Project = require('../models/project.model');
 
 const updateProject = async (req, res) => {
   try {
-    const projectID = req.body.fetchData;
-    //console.log(projectData);
-    const newProject = new Project(projectData);
-    console.log(newProject);
-    await newProject.save();
-    res.status(201).send(newProject);
+    const { projectId, updateData } = req.body;
+
+    // Find the project by ID and update it with the new data
+    const updatedProject = await Project.findByIdAndUpdate(projectId, updateData, { new: true });
+
+    if (!updatedProject) {
+      return res.status(404).send({ message: 'Project not found' });
+    }
+
+    res.status(200).send(updatedProject);
   } catch (error) {
-    console.error('Error saving project:', error);
+    console.error('Error updating project:', error);
     res.status(400).send({ error: error.message });
   }
 };
